73e6e51883491a45e36c5fb252823957f4e2046d
Lawrence Macharia <macharialawrence2011@gmail.com>
2018-02-14 07:53:00 +0300

    - triggering a custom event from child to parent

--- src/components/ChildOne.vue
+++ src/components/ChildOne.vue
@@ -2,27 +2,40 @@
     <div id="child-1">
         <h3 class="title">{{ title }}</h3>
         <p>{{ message }}</p>
+        <input type="text" placeholder="Write something..." v-model="something">
+        <button @click="submit">Submit</button>
     </div>
 </template>
 
 <script>
     export default{
         name: 'child-one',
-        props:['message'],
+        props: ['message'],
         data: function () {
             return {
-                'title': 'Child 1'
+                title: 'Child 1',
+                something: ''
+
             }
 
+        },
+        methods: {
+            submit: function () {
+                if (this.something.length === 0) {
+                    return
+                }
+                this.$emit('somethingSent', this.something)
+            }
         }
     }
 </script>
 
 <style>
-    #child-1{
-        height: 150px;
+    #child-1 {
+        height: 200px;
         background-color: green;
         border: 3px solid black;
         margin: 5px;
+        padding: 5px;
     }
 </style>
